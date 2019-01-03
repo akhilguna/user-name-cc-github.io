310da6bab483a6fcb05df6bcd2308563e4c6d608
akhil <akhilguna96@gmail.com>
2019-01-03 14:43:27 +0530

    react

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,8 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(  element,document.getElementById('root'));
+
 });
+
+
